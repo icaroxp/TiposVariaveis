b6b26485ed259ce8dba5901d8c3a09b510609ffe
icaroxp <icaroxp2@gmail.com>
2023-08-27 11:50:20 -0300

    Separação de Operadores e Variaveis em pagina

---
+++ src/OperadoresLogicos.java
@@ -0,0 +1,37 @@
+public class OperadoresLogicos {
+    public static void main(String[] args)throws Exception{
+
+
+//teste de Operadores aritmeticos:
+int multiplicação = 20*13;
+int soma = 12+33;
+int subtração = 20-13;
+double divisão = 20/13F; //em double e em float, precisamos colocar (em double f ou F no final do valor e em float, (f ou F);
+int modulo = 18%3;  //aqui temos o restante de uma divisão, o resto dela
+String nomeCompleto = "Icaro" + " " + "Xavier" + " " + "Peixoto";
+
+System.out.println(nomeCompleto);
+System.out.println(multiplicação);
+System.out.println(soma);
+System.out.println(subtração);
+System.out.println(divisão);
+System.out.println(modulo);
+
+
+
+//aprendendo operadores (mudando o valor para positivo e negativo)
+int num = 5;
+num = - num;
+
+num = num*(-1);
+System.out.println(num);
+
+
+
+
+
+
+
+
+    }
+}
--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,3 +1,5 @@
+
+
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
     
@@ -24,17 +26,6 @@ public class TiposVariaveis {
 
 
 
-//teste de Operadores aritmeticos:
-        int multiplicação = 20*13;
-        int soma = 12+33;
-        int subtração = 20-13;
-        double divisão = 20/13F; //em double e em float, precisamos colocar (em double f ou F no final do valor e em float, (f ou F);
-
-        System.out.println(multiplicação);
-        System.out.println(soma);
-        System.out.println(subtração);
-        System.out.println(divisão);
-    
 
         
 }
