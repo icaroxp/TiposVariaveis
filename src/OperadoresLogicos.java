a35c9f1f9ef23849c3bc09d13e5fd2ff7fd41231
icaroxp <icaroxp2@gmail.com>
2023-08-27 12:18:56 -0300

    aplicação de uso Ternário

--- src/OperadoresLogicos.java
+++ src/OperadoresLogicos.java
@@ -37,14 +37,34 @@ System.out.println(-- num);//decrementando em -1 quando o programa rodar
 
 boolean va = true; //aqui eu disse que a boolean va é true;
 
-System.out.println(!va); //aqui eu disse que a boolean va vai imprimir que va é false (pois usei a inversão dentro do print)
+System.out.println(!va); //aqui eu disse que a boolean va vai imprimir que va é false (pois usei a inversão dentro do print, dentro da memoria va ainda continua sendo true, mudando apenas no print)
 
-va = !va; //agora eu disse que nossa variavel passou de fato a sert true, ivnertemos o valor da variavel do tipo booleana;
+va = !va; //agora eu disse que nossa variavel passou de fato a ser false, ivnertemos o valor da variavel do tipo booleana;
 
 System.out.println(va);
 
+//agora vamos abreviar com uso de operador alternário:
 
+/*Por exemplo, quando queremos dizer que, se a for igual a b, uma mensagem será exbibida como (verdadeiro caso for verdade),
+ * e caso fou falso (falso);
+ * normalmente diriamos: EX: a=5; b=6;
+ * 
+ * String resultado = "";
+ * if(a==b)
+ *  resultado = "veradeiro";
+ * else
+ *  resultado="falso";
+ * System.out.println(resultado);
+ * 
+ * Assim, estaria correto, entretanto temos forma de colocar isso abreviadamente, como exemplo abaixo:
+ */
 
+    
+    int a = 5; int b = 6;
+
+        String resultado = a==b ?"Verdadeiro" : "falso";
+
+        System.out.println(resultado);
 
 
 
